31d953f9b17115037c531a3a846ac11c124ee5f9
Garima Saxena <GarimaSaxena0702@gmail.com>
2019-01-27 20:59:14 +0530

    efficiency

--- backdrop.js
+++ backdrop.js
@@ -1,52 +1,66 @@
 var allFoodPoints = [];
 var filteredFoodPoints = [];
 
-var canvas = document.getElementById('backCanvas');
-var bgCtx = canvas.getContext('2d');
+var canvas = document.getElementById("backCanvas");
+var bgCtx = canvas.getContext("2d");
 
-function drawBackdrop(){
-    allFoodPoints = [];
-    flushCanvas(bgCtx)
-    definePacFood(bgCtx)
+definePacFood(bgCtx);
 
-    GRID_1.forEach(gridRect => {
-        removeFoodPointsInRect(...gridRect)
-        roundedRect(bgCtx, ...gridRect)
-    })
-    drawPacFood(bgCtx)
+GRID_1.forEach(gridRect => {
+  removeFoodPointsInRect(...gridRect);
+});
 
+function drawBackdrop() {
+  bgCtx.clearRect(0, 0, 800, 800);
+  GRID_1.forEach(gridRect => {
+    roundedRect(bgCtx, ...gridRect)
+  });
+
+  drawPacFood(bgCtx);
 }
 
 drawBackdrop();
 
 function definePacFood(ctx) {
-    for (var i = 0; i < canvas.width; i += GRID_CELL_SIZE) {
-        for (var j = 0; j < canvas.width; j += GRID_CELL_SIZE) {
-            if (!filteredFoodPoints.includes([GRID_CELL_SIZE / 2 + i, GRID_CELL_SIZE / 2 + j].toString()))
-                allFoodPoints.push([GRID_CELL_SIZE / 2 + i, GRID_CELL_SIZE / 2 + j].toString());
-        }
+  for (var i = 0; i < canvas.width; i += GRID_CELL_SIZE) {
+    for (var j = 0; j < canvas.width; j += GRID_CELL_SIZE) {
+      if (
+        !filteredFoodPoints.includes(
+          [GRID_CELL_SIZE / 2 + i, GRID_CELL_SIZE / 2 + j].toString()
+        )
+      )
+        allFoodPoints.push(
+          [GRID_CELL_SIZE / 2 + i, GRID_CELL_SIZE / 2 + j].toString()
+        );
     }
+  }
 }
 
 function drawPacFood(ctx) {
-    for (var i = 0; i < allFoodPoints.length; i++) {
-        if (!filteredFoodPoints.includes(allFoodPoints[i])) {
-            ctx.fillRect(allFoodPoints[i].split(',').map(x => parseInt(x))[0], allFoodPoints[i].split(',').map(x => parseInt(x))[1], FOOD_SIZE, FOOD_SIZE);
-        }
+  for (var i = 0; i < allFoodPoints.length; i++) {
+    if (!filteredFoodPoints.includes(allFoodPoints[i])) {
+      ctx.fillRect(
+        allFoodPoints[i].split(",").map(x => parseInt(x))[0],
+        allFoodPoints[i].split(",").map(x => parseInt(x))[1],
+        FOOD_SIZE,
+        FOOD_SIZE
+      );
     }
+  }
 }
 
 function removeFoodPointsInRect(x, y, width, height) {
-    if (x || y) {
-        for (var i = 0; i < allFoodPoints.length; i++) {
-            foodX = allFoodPoints[i].split(',').map(x => parseInt(x))[0];
-            foodY = allFoodPoints[i].split(',').map(x => parseInt(x))[1];
-            if (
-                (foodX >= x && foodX <= x + width) &&
-                (foodY >= y && foodY <= y + height)
-            ) {
-                filteredFoodPoints.push(allFoodPoints[i]);
-            }
-        }
+  if (x || y) {
+    for (var i = 0; i < allFoodPoints.length; i++) {
+      foodX = allFoodPoints[i].split(",").map(x => parseInt(x))[0];
+      foodY = allFoodPoints[i].split(",").map(x => parseInt(x))[1];
+      if (
+        foodX >= x &&
+        foodX <= x + width &&
+        (foodY >= y && foodY <= y + height)
+      ) {
+        filteredFoodPoints.push(allFoodPoints[i]);
+      }
     }
+  }
 }
